f0e1dff597e202e17ed0805746138788c2d0a121
manishboro <officialmanishboro@gmail.com>
2021-10-27 21:34:24 +0530

    added prod api route

--- client/src/context/SocketContext.tsx
+++ client/src/context/SocketContext.tsx
@@ -54,7 +54,7 @@ const AppCtx = React.createContext<AppContextInterface | null>(null);
 
 export const useSocketContext = () => React.useContext(AppCtx);
 
-const socket = io("http://localhost:3001");
+const socket = io("https://video-chat-app-02.herokuapp.com/");
 
 const SocketContextProvider: React.FC = ({ children }) => {
   const alert = useAlertContext();
