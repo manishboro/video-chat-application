d2409cfe61e88d031416f4c8501fc03f4c30d7df
manishboro <officialmanishboro@gmail.com>
2021-11-03 12:39:25 +0530

    made changes in gitignore

--- server/build/app.js
+++ server/build/app.js
@@ -1,55 +1,78 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.httpServer = void 0;
+var dotenv = __importStar(require("dotenv"));
 var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
 var path_1 = __importDefault(require("path"));
-var http_1 = require("http");
 var socket_io_1 = require("socket.io");
+dotenv.config({ path: "./config.env" });
+if (!process.env.PORT)
+    process.exit(1);
+console.log("Environment :", process.env.NODE_ENV);
 var app = (0, express_1.default)();
-exports.httpServer = (0, http_1.createServer)(app);
-var io = new socket_io_1.Server(exports.httpServer, {
+var PORT = parseInt(process.env.PORT, 10);
+var server = app.listen(PORT, function () { return console.log("Running on port " + PORT); });
+var io = new socket_io_1.Server(server, {
     cors: { origin: "*", methods: ["GET", "POST"] },
 });
 app.use((0, cors_1.default)(), express_1.default.json());
-app.use(express_1.default.static(path_1.default.resolve(__dirname, "../client/build")));
+app.use(express_1.default.static(path_1.default.resolve(__dirname, "../../client/build")));
 app.get("/api", function (req, res) { return res.send("<h1>API server is running!!</h1>"); });
 app.get("*", function (req, res) {
-    res.sendFile(path_1.default.resolve(__dirname, "../client/build", "index.html"));
+    res.sendFile(path_1.default.resolve(__dirname, "../../client/build", "index.html"));
 });
 io.on("connection", function (socket) {
-    // Sends the socket ID of the connected user to the client
     socket.emit("mySocketId", socket.id);
     socket.on("join-room", function (roomName) {
-        // Join a room
         socket.join(roomName);
-        // Get all sockets connected to different rooms
         var allRooms = io.of("/").adapter.rooms;
-        // Get sockets connected to a particular room
         var myRoom = allRooms.get(roomName);
-        // Sending room details to all the connected client even to myself
-        io.to(roomName).emit("user-connected", { roomId: roomName, myRoom: Array.from(myRoom !== null && myRoom !== void 0 ? myRoom : []) }); // Converting myRoom Set to an Array
+        io.to(roomName).emit("user-connected", {
+            roomId: roomName,
+            myRoom: Array.from(myRoom !== null && myRoom !== void 0 ? myRoom : []),
+        });
     });
-    // Send my information to the person whom we are calling
-    socket.on("callUser", function (_a) {
-        var userToCall = _a.userToCall, signalData = _a.signalData, from = _a.from, displayName = _a.displayName;
-        return io.to(userToCall).emit("listenForCall", {
-            signal: signalData,
-            from: from,
+    socket.on("call-user", function (_a) {
+        var userToCall = _a.userToCall, sdpOffer = _a.sdpOffer, callerId = _a.callerId, displayName = _a.displayName;
+        io.to(userToCall).emit("listen-for-call", {
+            sdpOffer: sdpOffer,
+            callerId: callerId,
             displayName: displayName,
         });
     });
-    // socket.on("answerCall", (data) => io.to(data.to).emit("callAccepted", data.signal));
-    socket.on("answerCall", function (_a) {
-        var caller = _a.caller, receiverId = _a.receiverId, signalData = _a.signalData, displayName = _a.displayName;
-        return io.to(caller).emit("callAccepted", {
-            signal: signalData,
+    socket.on("answer-call", function (_a) {
+        var caller = _a.caller, receiverId = _a.receiverId, sdpAnswer = _a.sdpAnswer, displayName = _a.displayName;
+        return io.to(caller).emit("call-accepted", {
+            sdpAnswer: sdpAnswer,
             receiverId: receiverId,
             displayName: displayName,
         });
+    });
+    socket.on("new-ice-candidate", function (data) {
+        if (data.iceCandidate && data.to)
+            io.to(data.to).emit("add-ice-candidate", data);
     });
     socket.on("updateMyMedia", function (_a) {
         var type = _a.type, currentMediaStatus = _a.currentMediaStatus;
