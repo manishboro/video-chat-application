19cfce76e3e2b5962b9b55eae82c2ce9bdfd9617
manishboro <officialmanishboro@gmail.com>
2021-11-16 17:04:03 +0530

    made call form

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@mui/system";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./App";
 import "./styles/styles.css";
 import theme from "./styles/theme";
 
