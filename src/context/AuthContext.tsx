f4bfa7819418315444d5eac511fc25bfb1abf98a
manishboro <officialmanishboro@gmail.com>
2022-02-04 14:21:50 +0530

    removed auth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 // import AuthForm from "../components/auth-form";
 
-import { useAlertContext } from "./AlertContext";
+// import { useAlertContext } from "./AlertContext";
 
 const AuthContextProvider: React.FC = ({ children }) => {
-  const alert = useAlertContext();
-  const [authorized, setAuthorized] = React.useState(false);
+  // const alert = useAlertContext();
+  // const [authorized, setAuthorized] = React.useState(false);
 
   // return (
   //   <>
