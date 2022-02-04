c7fdd36bd02127fd51ddc8592c97f9417cf95815
manishboro <officialmanishboro@gmail.com>
2022-02-04 12:20:31 +0530

    remove auth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AuthForm from "../components/auth-form";
+// import AuthForm from "../components/auth-form";
 
 import { useAlertContext } from "./AlertContext";
 
@@ -7,15 +7,17 @@ const AuthContextProvider: React.FC = ({ children }) => {
   const alert = useAlertContext();
   const [authorized, setAuthorized] = React.useState(false);
 
-  return (
-    <>
-      {process.env.NODE_ENV === "development" || authorized ? (
-        children
-      ) : (
-        <AuthForm alert={alert} setAuthorized={setAuthorized} />
-      )}
-    </>
-  );
+  // return (
+  //   <>
+  //     {process.env.NODE_ENV === "development" || authorized ? (
+  //       children
+  //     ) : (
+  //       <AuthForm alert={alert} setAuthorized={setAuthorized} />
+  //     )}
+  //   </>
+  // );
+
+  return <>{children}</>;
 };
 
 export default AuthContextProvider;
