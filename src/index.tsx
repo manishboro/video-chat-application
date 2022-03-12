72a8eb0c0de31b07588e0a1aba81b46049520cea
manishboro <officialmanishboro@gmail.com>
2022-03-12 19:24:24 +0530

    disabled video

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// eslint-disable-next-line
+import adapter from "webrtc-adapter";
 import "./styles/index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
